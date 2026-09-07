a4065cefd7313cdc9543829ec0b385385baa4542
ripperd-2882 <ripperd2882@gmail.com>
2026-09-07 21:34:11 +0530

    Initial commit

---
+++ app/layout.js
@@ -0,0 +1,42 @@
+import { Josefin_Sans } from "next/font/google";
+
+import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
+import { ReservationProvider } from "./_components/ReservationsContext";
+
+const josafin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata = {
+  // title: "The Wild Oasis",
+  title: {
+    template: "%s | The Wild Oasis",
+    default: "Welcome | The Wild Oasis",
+  },
+
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${josafin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
+
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">
+            <ReservationProvider>
+              {children}{" "}
+              {/* whatever is appearing in the children has already been rendered on the server */}
+            </ReservationProvider>
+          </main>
+        </div>
+      </body>
+    </html>
+  );
+}
